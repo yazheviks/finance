3fff97e513911563136655030617499b71e63c82
Viktoriia Serhieieva <Viktoriia.Serhieieva@twoaeon.com>
2021-07-08 17:07:29 +0300

    logout

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {LoginComponent} from "./components/auth/login/login.component";
-import {RegistrationComponent} from "./components/auth/registration/registration.component";
-import {MainComponent} from "./components/site-layout/main/main.component";
-import {AuthGuard} from "./guards/auth.guard";
-import {AuthComponent} from "./components/auth/auth.component";
-import {SiteLayoutComponent} from "./components/site-layout/site-layout.component";
+import {LoginComponent} from './components/auth/login/login.component';
+import {RegistrationComponent} from './components/auth/registration/registration.component';
+import {MainComponent} from './components/site-layout/main/main.component';
+import {AuthGuard} from './guards/auth.guard';
+import {AuthComponent} from './components/auth/auth.component';
+import {SiteLayoutComponent} from './components/site-layout/site-layout.component';
 
 const routes: Routes = [
   { path: '', component: AuthComponent, children: [
--- client/src/app/components/site-layout/site-layout.component.ts
+++ client/src/app/components/site-layout/site-layout.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-site-layout',
   templateUrl: './site-layout.component.html',
   styleUrls: ['./site-layout.component.scss']
 })
-export class SiteLayoutComponent implements OnInit {
+export class SiteLayoutComponent {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {
+  logOut(): void {
+    console.log('here');
+    this.authService.logout();
+    this.router.navigate(['/login']);
   }
-
 }
